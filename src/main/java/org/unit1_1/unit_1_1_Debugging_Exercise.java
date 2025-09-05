61e604fd7abb27102bf072fa2eb47f26057f95de
Daniel H <danielimanihalter@gmail.com>
2025-09-05 09:35:26 -0400

    initial commit, completed up to 2nd on second page of the 1.1 debugging ws

---
+++ src/main/java/org/unit1_1/unit_1_1_Debugging_Exercise.java
@@ -0,0 +1,11 @@
+package org.unit1_1;
+
+public class unit_1_1_Debugging_Exercise {
+    /*
+     * This is a block comment.
+     */
+    public static void main (String[] args) {
+        // Generate some simple output
+        System.out.println("Hello World!");
+    }
+}
